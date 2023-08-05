c4cd7a7c856f851f14bfc7c9a2e98add6a70dc17
Abulfadl Ah <abulfadl.ahmadi@gmail.com>
2023-08-05 23:32:04 +0330

    create test data

--- word/cutpaper-gen/cutPaper-gen.py
+++ word/cutpaper-gen/cutPaper-gen.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfWriter, PdfReader
 import io
-# import treepoem
+import treepoem
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
@@ -8,29 +8,31 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 
 existing_pdf = PdfReader(
-    open("blank.pdf", "rb"))
+    open("cut-coded-py-001.pdf", "rb"))
 output = PdfWriter()
 
 pdfmetrics.registerFont(TTFont('Fira', 'FiraCode.ttf'))
 
 for i in range(10):
-    j=i+1
+    j = i+1
+    k = str(1001 + i)[-3:]
+    file_name = f"T{k}"
     print(f"Page {j}\t", end='')
     packet1 = io.BytesIO()
     can1 = canvas.Canvas(packet1, pagesize=A4)
     can1.setFont('Fira', size=12)
     can1.drawCentredString(33.7, 803, "T")
     can1.drawCentredString(54.1, 803, "0")
-    can1.drawCentredString(74.5, 803, "1")
+    can1.drawCentredString(74.5, 803, "2")
     can1.drawCentredString(94.9, 803, f"{i}")
 
-    # image = treepoem.generate_barcode(
-    #     barcode_type="qrcode",  # One of the BWIPP supported codes.
-    #     data=f"192.168.1.7:8000/cut/B10{i}",
-    # )
-    # image.convert("1").save("barcode.png")
-    # can1.drawImage('barcode.png', 20, 704.68,
-    #                width=31, height=31, mask='auto')
+    image = treepoem.generate_barcode(
+        barcode_type="qrcode",  # One of the BWIPP supported codes.
+        data=f"https://gh-ahmadi.github.io/financial-v2/cut/{file_name}.html",
+    )
+    image.convert("1").save("barcode.png")
+    can1.drawImage('barcode.png', 20, 704.68,
+                   width=31, height=31, mask='auto')
 
     can1.save()
     packet1.seek(0)
